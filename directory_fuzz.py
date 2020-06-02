ef0aaed498f5d08f81814c72c59b6a204ff363e4
Alsalt Alkharosi <alsaltalkharosi7@gmail.com>
2020-06-02 14:35:08 +0400

    Update directory_fuzz.py

--- directory_fuzz.py
+++ directory_fuzz.py
@@ -1,7 +1,20 @@
 # Written by: Alsalt Alkharosi
 #!/usr/bin/python
 
+#!/usr/bin/python
+
 import sys,requests
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 target = sys.argv[1]
 file_name = sys.argv[2]
@@ -14,9 +27,10 @@ for x in wordlist.readlines():
         url = 'http://'+str(target)+'/'+directory+'/'
         r = requests.get(url)
         if r.status_code==200:
-            print('[+]'+url+'\n')
-            output.write('[+]'+url+'\n')
+            print(bcolors.OKGREEN+'[+]'+url+':'+'\n'+bcolors.ENDC)
+            output.write(bcolors.OKGREEN+'[+]'+url+'\n'+bcolors.ENDC)
         else:
-            pass
+            print(bcolors.FAIL+'[-]'+url+bcolors.ENDC)
     except Exception as e:
         print(e)
+
