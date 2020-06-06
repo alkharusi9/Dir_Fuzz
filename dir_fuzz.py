c732f68b6147c0cca4a450555d402ba39c3cd834
Alsalt Alkharosi <alsaltalkharosi7@gmail.com>
2020-06-06 10:39:44 +0400

    Update dir_fuzz.py

--- dir_fuzz.py
+++ dir_fuzz.py
@@ -32,28 +32,31 @@ print(bcolors.FAIL+"""%s
                 # Coded By Alsalt Alkharosi - @0x_pwner
     """+bcolors.ENDC)
 
+def Scanner():
+    target = sys.argv[1]
+    file_name = sys.argv[2]
+    wordlist = open(file_name,'r')
+    output = open('targets.vcs','w')
+
+    for x in wordlist.readlines():
+        try:
+            directory = x.strip('\n')
+            url = 'http://'+str(target)+'/'+directory+'/'
+            r = requests.get(url)
+            if r.status_code==200:
+                print(bcolors.OKGREEN+'[+]'+url+':'+'\n'+bcolors.ENDC)
+                output.write(bcolors.OKGREEN+'[+]'+url+'\n'+bcolors.ENDC)
+            else:
+                print(bcolors.FAIL+'[-]'+url+bcolors.ENDC) # You cuold change this to 'pass' if you think the output is confusing.
+        except Exception as e:
+                print(e)
+
+        except KeyboardInterrupt:
+                print('[-] You clicked CTRL+C to kill the process')
+                break
 
 
-target = sys.argv[1]
-file_name = sys.argv[2]
-wordlist = open(file_name,'r')
-output = open('targets.vcs','w')
-
-for x in wordlist.readlines():
-    try:
-        directory = x.strip('\n')
-        url = 'http://'+str(target)+'/'+directory+'/'
-        r = requests.get(url)
-        if r.status_code==200:
-            print(bcolors.OKGREEN+'[+]'+url+':'+'\n'+bcolors.ENDC)
-            output.write(bcolors.OKGREEN+'[+]'+url+'\n'+bcolors.ENDC)
-        else:
-            print(bcolors.FAIL+'[-]'+url+bcolors.ENDC)
-    except Exception as e:
-        print(e)
-
-    except KeyboardInterrupt:
-        print('[-] You clicked CTRL+C to kill the process')
-        break
+if __name__=='__main__':
+    Scanner()
 
 
